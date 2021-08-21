bb4088927388422bbc0748870c51b7d68056467d
beulah <beulah@missmalini.com>
2021-08-21 20:05:45 +0530

    Added Styles for List Detail Screen

--- src/navigation/Navigator.js
+++ src/navigation/Navigator.js
@@ -3,7 +3,7 @@ import { View } from 'react-native'
 import AuthNavigation from './AuthNavigation'
 import HomeNavigator from './HomeNavigation';
 const RootNavigator = () => {
-    const isLoggnied = false
+    const isLoggnied = true
     return (
         <View style={{ flex: 1, backgroundColor: 'green' }} >
             {!isLoggnied ?
